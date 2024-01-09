583a44e02a08d1e4c9635a3a04bea3b00ff3e695
ori <mymail@gmail.com>
2024-01-09 20:35:24 +0200

    feat(PUT method by ID)

--- src/controllers/base_controller.ts
+++ src/controllers/base_controller.ts
@@ -13,16 +13,16 @@ export class BaseController<ModelType>{
         try {
             if (req.query.category) {
 
-                const objects = await this.model.find({ name: req.query.category });
+                const objects = await this.model.find({ category: req.query.category });
                 res.send(objects);
 
             } else if (req.query.userid) {
 
-                const objects = await this.model.find({ name: req.query.userid });
+                const objects = await this.model.find({ userid: req.query.userid });
                 res.send(objects);
 
             } else {
-                
+
                 const objects = await this.model.find();
                 res.send(objects);
             }
@@ -52,8 +52,14 @@ export class BaseController<ModelType>{
         }
     }
 
-    putById(req: Request, res: Response) {
+    async putById(req: Request, res: Response) {
         res.send("put by id: " + req.params.id);
+        try {
+            const obj = await this.model.findByIdAndUpdate(req.params.id, req.body);
+            res.status(200).send(obj);
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     deleteById(req: Request, res: Response) {
