1e071dff043506362902f01d5a89c8652a090685
28ori <282ori@gmail.com>
2024-03-11 21:24:47 +0200

    set deployment stuff

--- src/server.ts
+++ src/server.ts
@@ -27,9 +27,9 @@ initApp().then((app) => {
     } else {
         console.log("PRODUCTION");
         const options2 = {
-            key: fs.readFileSync("../client-key.pem"),
-            cert: fs.readFileSync("../client-cert.pem"),
-        };
-        https.createServer(options2, app).listen(process.env.HTTPS_PORT);
+            key: fs.readFileSync("./client-key.pem"),
+            cert: fs.readFileSync("./client-cert.pem")
+          };
+          https.createServer(options2, app).listen(process.env.HTTPS_PORT);
     }
 });
